bf5a9f89f138ed29e8460e2bdd0fb027495d8d2a
PEMF Nashville <nodrinksonthepiano@users.noreply.github.com>
2026-09-06 18:32:34 -0500

    Add homepage organization structured data

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,53 @@ import FAQ from '@/components/FAQ'
 import FinalCTA from '@/components/FinalCTA'
 import Footer from '@/components/Footer'
 
+const structuredData = {
+  '@context': 'https://schema.org',
+  '@graph': [
+    {
+      '@type': 'WebSite',
+      '@id': 'https://www.pemfnashville.com/#website',
+      url: 'https://www.pemfnashville.com/',
+      name: 'PEMF Nashville',
+      publisher: {
+        '@id': 'https://www.pemfnashville.com/#organization',
+      },
+    },
+    {
+      '@type': 'Organization',
+      '@id': 'https://www.pemfnashville.com/#organization',
+      name: 'PEMF Nashville',
+      url: 'https://www.pemfnashville.com/',
+      logo: 'https://www.pemfnashville.com/images/PEMFNASHVILLELOGO42026.jpg',
+      telephone: '+1-615-669-2391',
+      email: 'lisa@pemfnashville.com',
+      description:
+        'Nurse-led PEMF wellness sessions for muscle and joint comfort, workout recovery, and relaxation, available in-home across Nashville and at Kinnection Clinic in Brentwood.',
+      sameAs: [
+        'https://www.instagram.com/pemfnashville/',
+        'https://www.facebook.com/pemfnashville',
+      ],
+      areaServed: [
+        'Nashville, TN',
+        'Brentwood, TN',
+        'Belle Meade, TN',
+        'Green Hills, TN',
+        'Franklin, TN',
+        'Bellevue, TN',
+      ],
+    },
+  ],
+}
+
 export default function Home() {
   return (
     <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(structuredData).replace(/</g, '\\u003c'),
+        }}
+      />
       <Header />
       <main>
         <Hero />
